c6b479a4c5a3b88f4d79e98c38407e8777110fa2
会飞面包 <872877904@qq.com>
2019-11-03 11:00:34 +0800

    11.3

--- src/components/home/HomeBook.vue
+++ src/components/home/HomeBook.vue
@@ -3,30 +3,60 @@
     <div class="home-book-header">{{title}}</div>
     <div class="home-book-content">
       <div class="home-book-row" v-for="(item, index) in bookData" :key="index">
-        <div class="home-book-col" v-for="(book, bookIndex) in item" :key="bookIndex">
+        <div
+          class="home-book-col"
+          v-for="(book, bookIndex) in item" :key="bookIndex"
+          :style="{ flex: '0 0' + (100/col) + '%' }"
+        >
           <div
             class="book-wrapper"
-            :style="{ flex: '0 0' + (100/col) + '%' }"
+            :style="{ flexDirection: mode === HOME_BOOK_MODE.COL ? 'column' : 'row' }"
+            @click="onBookClick(book)"
+            v-if="mode === HOME_BOOK_MODE.COL || mode === HOME_BOOK_MODE.ROW"
           >
             <ImageView :src="book.cover"/>
-            <div class="book-title-wrapper book-title-col">
+            <div
+              class="book-title-wrapper book-title-col"
+              v-if="mode === HOME_BOOK_MODE.COL"
+            >
               <div class="book-title">{{book.title}}</div>
+            </div>
+            <div class="book-title-wrapper book-title-row">
+              <div class="book-title">{{book.title}}</div>
+              <div class="book-title-author-wrapper">
+                <div class="book-title book-author">{{book.author}}</div>
+                <div class="book-title book-author">{{book.categoryText}}</div>
+              </div>
+            </div>
+          </div>
+          <div class="category-wrapper" v-else @click="onBookClick(book)">
+            <div class="category-text">{{book.text}}</div>
+            <div class="category-num">{{book.num}}本书</div>
+            <div class="category-img-wrapper">
+              <div class="category-img1">
+                <ImageView :src="book.cover"></ImageView>
+              </div>
+              <div class="category-img2">
+                <ImageView :src="book.cover2"></ImageView>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <div class="home-book-footer">3</div>
+    <div class="home-book-footer" v-if="showBtn" @click="onMoreClick">
+      <van-button round custom-class="home-book-btn">{{btnText}}</van-button>
+    </div>
   </div>
 </template>
 
 <script>
-  import {HOME_BOOK_MODE} from '@/utils/const'
+  import {HOME_BOOK_MODE, CATEGORY} from '@/utils/const'
   import ImageView from '../base/ImageView'
 
   export default {
     name: 'HomeBook',
-    components: { ImageView },
+    components: {ImageView},
     mounted() {
       console.log(this.bookData)
     },
@@ -63,9 +93,15 @@
       }
     },
     computed: {
+      HOME_BOOK_MODE() {
+        return HOME_BOOK_MODE
+      },
       bookData() {
-        const { data, row, col } = this
+        const {data, row, col} = this
         if (data && data.length > 0) {
+          data.forEach(book => {
+            book.text = CATEGORY[book.categoryText.toLowerCase()]
+          })
           const number = row * col
           const _bookData = data.slice(0, number)
           const _bookDataRow = []
@@ -96,15 +132,22 @@
     .home-book-header {
       padding: 13px 0 0 20.5px;
     }
+
     .home-book-content {
       padding: 0 12px;
-      margin-top: 22.5px;
+      margin-top: 10.5px;
+
       .home-book-row {
         display: flex;
         flex-flow: row nowrap;
+        margin-top: 12px;
+
         .home-book-col {
           padding: 0 8px;
+          box-sizing: border-box;
+
           .book-wrapper {
+            display: flex;
             .book-title-wrapper {
               &.book-title-col {
                 .book-title {
@@ -117,10 +160,92 @@
                   word-break: break-word;
                 }
               }
+              &.book-title-row {
+                flex: 0 0 50%;
+                padding: 10px;
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+                .book-title {
+                  font-size: 14px;
+                  color: #1F1F1F;
+                  line-height: 18px;
+                  max-height: 36px;
+                  overflow: hidden;
+                  word-break: break-word;
+                }
+                .book-author {
+                  font-size: 12px;
+                  color: #868686;
+                  line-height: 14px;
+                  max-height: 14px;
+                  overflow hidden
+                }
+              }
+            }
+          }
+          .category-wrapper {
+            position: relative;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            background: #F8F9FB;
+            border-radius: 10px;
+            height: 96px;
+            padding: 13px 0 14.5px 16px;
+            box-sizing: border-box;
+
+            .category-text {
+              width: 150px;
+              overflow: hidden;
+              text-overflow: ellipsis;
+              color: #212832;
+              font-size: 16px;
+              line-height: 22.5px;
+            }
+            .category-num {
+              color: #868686;
+              font-size: 12px;
+              line-height: 16.5px;
+            }
+            .category-img-wrapper {
+              position: absolute;
+              right: 0;
+              bottom: 0;
+
+              .category-img1 {
+                position: absolute;
+                right: 0;
+                bottom: -5px;
+                z-index: 100;
+                width: 48px;
+                border-radius: 0 0 10px 0;
+              }
+              .category-img2 {
+                position: absolute;
+                right: 30px;
+                bottom: -5px;
+                z-index: 90;
+                width: 36px;
+              }
             }
           }
         }
       }
+    }
+
+    .home-book-footer {
+      padding: 12px 20px 20px;
+    }
+  }
+</style>
+<style lang="stylus">
+  .home-book-footer {
+    .home-book-btn {
+      width: 100%;
+      font-size: 14px;
+      color: #3696EF;
+      border: 1px solid #EDEEEE;
     }
   }
 </style>
